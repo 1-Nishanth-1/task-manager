1d69b209db36355134a1f3ae5f6704e01b7ca9be
1-Nishanth-1 <nishanthrn1@gmail.com>
2026-03-23 16:57:51 +0530

    fix: darkkk

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./features/**/*.{ts,tsx,js,jsx}",
+    "./lib/**/*.{ts,tsx,js,jsx}",
+  ],
+};
+
+export default config;
